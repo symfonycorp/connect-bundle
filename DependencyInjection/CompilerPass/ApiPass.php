b439a5e7d8356903fcc4bdc2dfda8e0ca4890215
Nicolas Grekas <nicolas.grekas@gmail.com>
2019-07-19 19:13:53 +0200

    Fixes

--- DependencyInjection/CompilerPass/ApiPass.php
+++ DependencyInjection/CompilerPass/ApiPass.php
@@ -7,6 +7,9 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 use SymfonyCorp\Connect\Api\Api;
 
+/**
+ * @internal since connect-bundle v5.1, to be removed in 6.0
+ */
 class ApiPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
